f5b4848396f33eebf172279fdf6b9fc1a59cc244
Ben McCann <ben@benmccann.com>
2017-08-10 04:45:57 -0700

    Align dataframe filter naming

--- datavec-dataframe/src/main/java/org/datavec/dataframe/filtering/StringInSet.java
+++ datavec-dataframe/src/main/java/org/datavec/dataframe/filtering/StringInSet.java
@@ -1,37 +1,16 @@
 package org.datavec.dataframe.filtering;
 
 import org.datavec.dataframe.api.CategoryColumn;
-import org.datavec.dataframe.api.ColumnType;
-import org.datavec.dataframe.api.Table;
-import org.datavec.dataframe.columns.Column;
 import org.datavec.dataframe.columns.ColumnReference;
-import org.datavec.dataframe.util.Selection;
-
-import java.util.Collection;
 
 /**
- * Implements EqualTo testing for Category and Text Columns
+ * @deprecated use {@link StringIsIn} instead.  
  */
-public class StringInSet extends ColumnFilter {
+@Deprecated
+public class StringInSet extends StringIsIn {
 
-    private Collection<String> values;
-
-    public StringInSet(ColumnReference reference, Collection<String> values) {
-        super(reference);
-        this.values = values;
+    public StringInSet(ColumnReference reference, CategoryColumn filterColumn) {
+        super(reference, filterColumn);
     }
 
-    public Selection apply(Table relation) {
-        Column column = relation.column(columnReference.getColumnName());
-        ColumnType type = column.type();
-        switch (type) {
-            case CATEGORY: {
-                CategoryColumn categoryColumn = (CategoryColumn) relation.column(columnReference.getColumnName());
-                return categoryColumn.isIn(values.toArray(new String[values.size()]));
-            }
-            default:
-                throw new UnsupportedOperationException(
-                                String.format("ColumnType %s does not support equalTo on a String value", type));
-        }
-    }
 }
--- datavec-dataframe/src/main/java/org/datavec/dataframe/filtering/StringNotInSet.java
+++ datavec-dataframe/src/main/java/org/datavec/dataframe/filtering/StringNotInSet.java
@@ -1,37 +1,16 @@
 package org.datavec.dataframe.filtering;
 
 import org.datavec.dataframe.api.CategoryColumn;
-import org.datavec.dataframe.api.ColumnType;
-import org.datavec.dataframe.api.Table;
-import org.datavec.dataframe.columns.Column;
 import org.datavec.dataframe.columns.ColumnReference;
-import org.datavec.dataframe.util.Selection;
-
-import java.util.Collection;
 
 /**
- * Implements EqualTo testing for Category and Text Columns
+ * @deprecated use {@link StringIsNotIn} instead.  
  */
-public class StringNotInSet extends ColumnFilter {
+@Deprecated
+public class StringNotInSet extends StringIsNotIn {
 
-    private Collection<String> values;
-
-    public StringNotInSet(ColumnReference reference, Collection<String> values) {
-        super(reference);
-        this.values = values;
+    public StringNotInSet(ColumnReference reference, CategoryColumn filterColumn) {
+        super(reference, filterColumn);
     }
 
-    public Selection apply(Table relation) {
-        Column column = relation.column(columnReference.getColumnName());
-        ColumnType type = column.type();
-        switch (type) {
-            case CATEGORY: {
-                CategoryColumn categoryColumn = (CategoryColumn) relation.column(columnReference.getColumnName());
-                return categoryColumn.isNotInSet(values);
-            }
-            default:
-                throw new UnsupportedOperationException(
-                                String.format("ColumnType %s does not support equalTo on a String value", type));
-        }
-    }
 }
